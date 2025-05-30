1dafabf69f076b78c899293f8b59f5d595752fa3
beejh02 <joon0448@naver.com>
2025-05-30 21:51:02 +0900

    [feat] queue.cpp : qtype.h의 필드추가를 위한 atomic 관리 제거

--- os2025-2nd-assignment/queue.cpp
+++ os2025-2nd-assignment/queue.cpp
@@ -65,45 +65,42 @@ Reply enqueue(Queue* queue, Item item) {
 	Node* new_node = nalloc(item);
 	if (new_node == NULL) return reply;
 
-	{
-		lock_guard<mutex> lock(queue->mtx);
-		reply.success = true;
-		reply.item = item;
+	reply.success = true;
+	reply.item = item;
 
-		// if first, point to the same thing(head, tail)
-		if (queue->head == NULL) {
-			queue->head = new_node;
+	// if first, point to the same thing(head, tail)
+	if (queue->head == NULL) {
+		queue->head = new_node;
+		queue->tail = new_node;
+		return reply;
+	}
+
+	// search, descending order
+	Node* current = queue->head;
+	Node* prev = NULL;
+	while (current != NULL && current->item.key > item.key) {
+		prev = current;
+		current = current->next;
+	}
+
+	if (prev == NULL) {
+		new_node->next = queue->head;
+		queue->head = new_node;
+
+		if (queue->tail == NULL) {
 			queue->tail = new_node;
-			return reply;
 		}
-
-		// search, descending order
-		Node* current = queue->head;
-		Node* prev = NULL;
-
-		while (current != NULL && current->item.key > item.key) {
-			prev = current;
-			current = current->next;
-		}
-
-		if (prev == NULL) {
-			new_node->next = queue->head;
-			queue->head = new_node;
-
-			if (queue->tail == NULL) {
-				queue->tail = new_node;
-			}
-		}
-		else {
-			new_node->next = current;
-			prev->next = new_node;
-			if (current == NULL) {
-				queue->tail = new_node;
-			}
+	}
+	else {
+		new_node->next = current;
+		prev->next = new_node;
+		if (current == NULL) {
+			queue->tail = new_node;
 		}
 	}
 
-	cv.notify_one();
+
+	//cv.notify_one();
 
 	return reply;
 }
@@ -112,8 +109,8 @@ Reply dequeue(Queue* queue) {
 	Reply reply = { false, {0, NULL} };
 	if (queue == NULL) return reply;
 
-	unique_lock<mutex> lock(mtx);
-	cv.wait(lock, [queue] {return queue->head != NULL; });
+	//unique_lock<mutex> lock(mtx);
+	//cv.wait(lock, [queue] {return queue->head != NULL; });
 
 	Node* temp = queue->head;
 	queue->head = queue->head->next;
@@ -136,18 +133,16 @@ Queue* range(Queue* queue, Key start, Key end) {
 
 	if (new_queue == NULL || queue == NULL) return new_queue;
 
-	{
-		lock_guard<mutex> lock(mtx);
-		Node* current = queue->head;
+	Node* current = queue->head;
 
-		// start <= key && key <= end
-		while (current != NULL) {
-			if (start <= current->item.key && current->item.key <= end) {
-				Item item = current->item;
-				enqueue(new_queue, item);
-			}
-			current = current->next;
+	// start <= key && key <= end
+	while (current != NULL) {
+		if (start <= current->item.key && current->item.key <= end) {
+			Item item = current->item;
+			enqueue(new_queue, item);
 		}
+		current = current->next;
+
 	}
 	return new_queue;
 }
